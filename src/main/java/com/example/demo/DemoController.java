d200fa892813c44324e20fa3efabb58577528be0
Luong Tran <luongtran@deputy.com>
2024-10-01 01:34:07 +0700

    fix: syntax

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -1,7 +1,7 @@
-package com.example.demo
+package com.example.demo;
 
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/demo")
 public class DemoController
