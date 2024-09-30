629db8346a6955b2baafa9368456804f76ccd3b4
Luong Tran <luongtran@deputy.com>
2024-09-30 23:55:54 +0700

    first commit

---
+++ src/main/java/com/example/demo/DemoController.java
@@ -0,0 +1,14 @@
+package com.example.demo
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController("/demo")
+public class DemoController
+{
+    @GetMapping("/greet")
+    public String index()
+    {
+        return "Greetings from Spring Boot!";
+    }
+}
