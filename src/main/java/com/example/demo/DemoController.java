448f124568dbaa074c5a880511a638a60971f18d
Luong Tran <luongtran@deputy.com>
2024-10-01 22:07:03 +0700

    update properties

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -3,12 +3,12 @@ package com.example.demo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/demo")
+@RestController
 public class DemoController
 {
-    @GetMapping("/greet")
+    @GetMapping("/")
     public String index()
     {
-        return "Greetings from Spring Boot!";
+        return "Greetings from Sydney!";
     }
 }
